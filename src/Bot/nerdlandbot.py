b2bcfa5e251d1dada232f1224bacc386ef7dc27f
wouter meuwis <wouter.meuwis@rmdy.be>
2020-05-21 12:42:28 +0200

    Added base setup

---
+++ src/Bot/nerdlandbot.py
@@ -0,0 +1,21 @@
+from discord.ext.commands import Bot
+from EventHandlers import onready, oncommanderror
+from Commands import rolldice
+
+
+class NerdlandBot(Bot):
+    def __init__(self, prefix):
+        self.prefix = prefix
+
+        super().__init__(command_prefix=self.prefix)
+
+        self.command(name='roll_dice')(self.roll_dice)
+
+    async def on_ready(self):
+        await onready.on_ready(self)
+
+    async def on_command_error(self, context, exception):
+        await oncommanderror.on_command_error(self, context, exception)
+
+    async def roll_dice(self, ctx, number_of_dice: int, number_of_sides: int):
+        await rolldice.roll(ctx, number_of_dice, number_of_sides)
