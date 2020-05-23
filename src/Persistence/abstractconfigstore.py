0fbb776855b383c091cd7186185bde12f77da4df
Wouter Hermans <wouter.hermans@gmail.com>
2020-05-23 17:05:27 +0200

    7-persistence: very basic dict-to-json-and-back config system

---
+++ src/Persistence/abstractconfigstore.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+
+class ConfigStore(ABC):
+
+    data = {}
+
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        pass
