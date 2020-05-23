66b570d52031b0a01f7d38dadce98b01e5d7142f
Wouter Hermans <wouter.hermans@gmail.com>
2020-05-23 17:14:20 +0200

    7-persistence: added type hinting

--- src/Persistence/configuration.py
+++ src/Persistence/configuration.py
@@ -1,24 +1,27 @@
+from Persistence.abstractconfigstore import ConfigStore
+
+
 class Configuration:
 
     config_store = None
 
-    def __init__(self, config_store):
+    def __init__(self, config_store: ConfigStore):
         self.config_store = config_store
 
     def __get_data(self):
         return self.config_store.data
 
-    def __set_data(self, data):
+    def __set_data(self, data: dict):
         self.config_store.data = data
 
     def refresh(self):
         self.__set_data(self.config_store.read())
 
-    def get(self, key):
+    def get(self, key: str):
         self.refresh()
         return self.__get_data()[key]
 
-    def put(self, key, value):
+    def put(self, key: str, value: str):
         self.refresh()
         self.__get_data()[key] = value
         self.config_store.write(self.__get_data())
