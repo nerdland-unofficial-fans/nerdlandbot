fdf3ed10a295094c8705f7224c178a5412cfc786
Eva774 <evasmeets94@gmail.com>
2020-05-27 17:39:10 +0200

    Update onmemberjoin.py

--- src/EventHandlers/onmemberjoin.py
+++ src/EventHandlers/onmemberjoin.py
@@ -4,9 +4,9 @@ member_notification_number = 100
 
 
 async def on_member_join(bot,member):
-  for guild in bot.guilds:
-    notification_channel_id = discord.utils.get(guild.channels,name=notification_channel_name).id
-    number_members = len(guild.members)
-    if number_members%member_notification_number == 0:
-      channel = bot.get_channel(notification_channel_id)
-      await channel.send('The current member count is {}'.format(number_members))
+  guild = member.guild
+  notification_channel_id = discord.utils.get(guild.channels,name=notification_channel_name).id
+  number_members = len(guild.members)
+  if number_members%member_notification_number == 0:
+    channel = bot.get_channel(notification_channel_id)
+    await channel.send('The current member count is {},guild{}'.format(number_members,member.guild))
