06a6139592c05966b7d068976913c202533c95cc
Vincent Van Beeumen <vincent.vanbeeumen@virteo.be>
2020-05-29 22:41:26 +0200

    WIP

--- src/Commands/notify.py
+++ src/Commands/notify.py
@@ -1,37 +1,45 @@
-notification_lists = dict()
+from serverData import loadServerData
 
 async def subscribe(ctx, list_name):
-    if list_name not in notification_lists.keys():
-        notification_lists[list_name] = []
+    data = loadServerData(ctx)
 
-    if ctx.author.id in notification_lists[list_name]:
+    if list_name not in data.notification_lists.keys():
+        data.notification_lists[list_name] = []
+
+    if ctx.author.id in data.notification_lists[list_name]:
         await ctx.send('You are already subscribed to this list')
     else:
-        notification_lists[list_name].append(ctx.author.id)
+        data.notification_lists[list_name].append(ctx.author.id)
         await ctx.send('Subscribed ' + ctx.author.nick + ' to ' + list_name)
 
 async def unsubscribe(ctx, list_name):
-    if list_name not in notification_lists.keys():
+    data = loadServerData(ctx)
+
+    if list_name not in data.notification_lists.keys():
         await ctx.Send('That list does not seem to exist, cannot unsubscribe')
     else:
-        if ctx.author.id in notification_lists[list_name]:
-            notification_lists[list_name].remove(ctx.author.id)
+        if ctx.author.id in data.notification_lists[list_name]:
+            data.notification_lists[list_name].remove(ctx.author.id)
             await ctx.send('Unsubscribed ' + ctx.author.nick + ' from ' + list_name)
         else:
             await ctx.Send('You dont seem to be subscribed to this list')
 
 async def notify(ctx, list_name):
-    if list_name not in notification_lists.keys():
+    data = loadServerData(ctx)
+
+    if list_name not in data.notification_lists.keys():
         await ctx.send('That list does not seem to exist')
     else:
-        if notification_lists[list_name]:
-            mentionList = ['<@' + str(id) + '>' for id in notification_lists[list_name]]
+        if data.notification_lists[list_name]:
+            mentionList = ['<@' + str(id) + '>' for id in data.notification_lists[list_name]]
             await ctx.send('Notifying ' + ', '.join(mentionList))
         else:
             await ctx.send('Nobody to notify')
 
 async def show_lists(ctx):
-    if notification_lists: 
-        await ctx.send(', '.join(notification_lists.keys()))
+    data = loadServerData(ctx)
+
+    if data.notification_lists: 
+        await ctx.send(', '.join(data.notification_lists.keys()))
     else:
-        await ctx.send('No lists exist yet')    
+        await ctx.send('No lists exist yet')
---
+++ src/Commands/serverData.py
@@ -0,0 +1,8 @@
+class ServerData():
+    notification_lists: dict()
+
+    def __init__(self):
+        self.notification_lists = dict()
+
+async def loadServerData(ctx):
+    pass
