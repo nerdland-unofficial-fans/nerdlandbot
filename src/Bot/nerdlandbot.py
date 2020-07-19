4bf7892da8d4a30c27315fa39c7aa9578095992d
Thunderb1rdOne <vincent.vanbeeumen@gmail.com>
2020-07-19 23:18:19 +0200

    remove unused code in nerdlandbot.py and refactor notify.py

--- src/Bot/nerdlandbot.py
+++ src/Bot/nerdlandbot.py
@@ -1,6 +1,6 @@
 from discord.ext.commands import Bot
 from EventHandlers import onready, oncommanderror, onmemberjoin
-# from Commands import notify
+
 
 
 class NerdlandBot(Bot):
@@ -8,12 +8,6 @@ class NerdlandBot(Bot):
         self.prefix = prefix
 
         super().__init__(command_prefix=self.prefix)
-
-        # self.command(name='sub')(self.subscribe)
-        # self.command(name='unsub')(self.unsubscribe)
-        # # self.command(name='notify')(self.notify)
-        # self.command(name='show_lists')(self.show_lists)
-        # self.command(name='save_config')(self.save_config)
 
     # EVENTS
     async def on_ready(self):
@@ -24,20 +18,3 @@ class NerdlandBot(Bot):
 
     async def on_member_join(self,member):
         await onmemberjoin.on_member_join(self,member)
-
-    # NOTIFICATIONS
-    # async def subscribe(self, ctx, list_name:str):
-    #     await notify.subscribe(ctx, list_name.lower())
-
-    # async def unsubscribe(self, ctx, list_name:str):
-    #     await notify.unsubscribe(ctx, list_name.lower())
-
-    # async def notify(self, ctx, list_name:str):
-    #     await notify.notify(ctx, list_name.lower())
-
-    # async def show_lists(self, ctx):
-    #     await notify.show_lists(ctx)
-
-    # async def save_config(self, ctx):
-    #     #TODO: We should probably add some way to only allow the person that hosts the bot to run this command
-    #     await notify.save_config(ctx)
