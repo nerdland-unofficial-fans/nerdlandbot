b66a3f1f07cbb945cb4229e0dbcfddb77c2854ab
Sebastiaan Jansen <whitebird@live.be>
2020-07-27 23:44:56 +0200

    First version of reaction based subbing
    Co-authored-by: Eva774
    Co-authored-by: Autom3

--- EventHandlers/oncommanderror.py
+++ EventHandlers/oncommanderror.py
@@ -3,4 +3,15 @@ from discord.ext import commands
 
 async def on_command_error(bot, ctx, error):
     print(error)
-    await ctx.send('Unrecognized command')
+    if isinstance(error, commands.MissingRequiredArgument):
+        command_name = ctx.message.content.split(" ")[0]
+        msg = (
+            "My command `"
+            + command_name
+            + "` requires more arguments. `"
+            + error.param.name
+            + "` is missing."
+        )
+    else:
+        msg = "Unrecognized command"
+    await ctx.send(msg)
