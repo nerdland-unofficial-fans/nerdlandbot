9276bbc68617a9edc23a3b7397e8b4d35e7fe18b
Sebastiaan Jansen <whitebird@live.be>
2020-07-27 13:18:01 +0200

    basic refactor of servData
    Co-authored-by: Autom3
    Co-authored-by: ThunderbirdOne

--- Commands/notify.py
+++ Commands/notify.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from .serverData import load_guild_config, save_configs
+from .serverData import get_guild_data, save_configs
 import discord
 
 
@@ -11,67 +11,43 @@ class Notify(commands.Cog):
     async def sub(self, ctx, list_name):
         list_name = list_name.lower()
 
-        config = await load_guild_config(ctx)
+        guild_data = await get_guild_data(ctx.message.guild.id)
+        msg_string = guild_data.sub(list_name, ctx.author.id)
 
-        if list_name not in config.notification_lists.keys():
-            await ctx.send("This list does not exist")
-            # TODO: print lists with reactions to sub
-        elif ctx.author.id in config.notification_lists[list_name]:
-            await ctx.send("You are already subscribed to this list")
-        else:
-            config.notification_lists[list_name].append(ctx.author.id)
-            config.guild_changed = True
-
-            await ctx.send("Subscribed " + ctx.author.nick + " to " + list_name)
+        await ctx.send(msg_string)
 
     @commands.command(name="unsub", aliases=["unsubscribe"])
     async def unsubscribe(self, ctx, list_name):
         list_name = list_name.lower()
 
-        config = await load_guild_config(ctx)
+        guild_data = await get_guild_data(ctx.message.guild.id)
+        msg_string = guild_data.unsub(list_name, ctx.author.id)
 
-        if list_name not in config.notification_lists.keys():
-            await ctx.Send("That list does not seem to exist, cannot unsubscribe")
-        else:
-            if ctx.author.id in config.notification_lists[list_name]:
-                config.notification_lists[list_name].remove(ctx.author.id)
-                config.guild_changed = True
-
-                await ctx.send("Unsubscribed " + ctx.author.nick + " from " + list_name)
-            else:
-                await ctx.Send("You dont seem to be subscribed to this list")
+        await ctx.send(msg_string)
 
     @commands.command(name="notify")
     async def notify(self, ctx, list_name):
         list_name = list_name.lower()
 
-        config = await load_guild_config(ctx)
+        guild_data = await get_guild_data(ctx.message.guild.id)
+        msg_string = guild_data.notify(list_name)
 
-        if list_name not in config.notification_lists.keys():
-            await ctx.send("That list does not seem to exist")
-        else:
-            if config.notification_lists[list_name]:
-                mentionList = [
-                    "<@" + str(id) + ">" for id in config.notification_lists[list_name]
-                ]
-                await ctx.send("Notifying " + ", ".join(mentionList))
-            else:
-                await ctx.send("Nobody to notify")
+        await ctx.send(msg_string)
 
     @commands.command(name="show_lists")
     async def show_lists(self, ctx):
-        config = await load_guild_config(ctx)
+        guild_data = await get_guild_data(ctx.message.guild.id)
 
-        if config.notification_lists:
+        if guild_data.notification_lists:
             msg = await ctx.send(
                 ", ".join(
                     [
                         v["icon"] + " - " + k
-                        for k, v in config.notification_lists.items()
+                        for k, v in guild_data.notification_lists.items()
                     ]
                 )
             )
-            for v in config.notification_lists.values():
+            for v in guild_data.notification_lists.values():
                 await msg.add_reaction(v["icon"])
 
             reaction, user = await ctx.bot.wait_for(
@@ -88,11 +64,11 @@ class Notify(commands.Cog):
 
     @commands.command(name="my_lists")
     async def my_lists(self, ctx):
-        config = await load_guild_config(ctx)
+        guild_data = await get_guild_data(ctx.message.guild.id)
         subbed_lists = []
 
-        if config.notification_lists:
-            for key, users_list in config.notification_lists.items():
+        if guild_data.notification_lists:
+            for key, users_list in guild_data.notification_lists.items():
                 if ctx.author.id in users_list:
                     subbed_lists.append(key)
 
