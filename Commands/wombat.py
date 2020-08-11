427dfb28d92cdba83c82526faa6e6425385d5738
Senne De Vocht <senne.de.vocht@telenet.be>
2020-08-11 10:31:43 +0200

    cleaned up code

--- Commands/wombat.py
+++ Commands/wombat.py
@@ -9,12 +9,13 @@ class Wombat(commands.Cog, name="Wombat"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="wombat_pic", brief="Post a random wombat picture", usage="", help="The bot will search in its database for a random piccture of a wombat, and post it in the chat")
-    async def sub(self, ctx):
+    @commands.command(name="wombat_pic", brief="Post a random wombat picture", help="The bot will search in its database for a random picture of a wombat in its database, and post it in the chat")
+    async def cmd_wombat_pic(self, ctx):
 
-        wombats = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + "\wombats\\"
+        parent_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+        wombats_dir = os.path.join(parent_dir, "wombats")
 
-        wombat_list = [wombats + w for w in os.listdir(wombats)]
+        wombat_list = [os.path.join(wombats_dir, w) for w in os.listdir(wombats_dir)]
 
         await ctx.send(file=discord.File(random.choice(wombat_list)))
 
