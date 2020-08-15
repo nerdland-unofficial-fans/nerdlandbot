4e8c0924a5e77ebc107d9b5ac75afc28990ee817
Bob van der Vleuten <bob.vandervleuten@gmail.com>
2020-08-15 11:19:32 +0200

    set groupsize to normal value

--- Helpers/constants.py
+++ Helpers/constants.py
@@ -1,5 +1,5 @@
 ESCAPE_MIN_GROUPSIZE = 3
-ESCAPE_MAX_GROUPSIZE = 2
+ESCAPE_MAX_GROUPSIZE = 6
 TIMEOUT = 60*5
 ESCAPE_COLOUR = 0x004080
 ESCAPE_THUMB = "https://previews.123rf.com/images/arcady31/arcady311810/arcady31181000064/109268325-escape-room-vector-icon.jpg"
