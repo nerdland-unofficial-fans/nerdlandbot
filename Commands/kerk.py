22a102fc337082a4c15e093fdc214dd5d743ecfe
Bob van der Vleuten <bob.vandervleuten@gmail.com>
2020-08-15 19:58:35 +0200

    adds optional message to kerk

--- Commands/kerk.py
+++ Commands/kerk.py
@@ -13,15 +13,17 @@ class Kerk(commands.Cog, name="kerk"):
         self.bot = bot
 
     @commands.command(name="16ukerk", hidden=True)
-    async def cmd_kerk16(self, ctx: commands.Context, mention:typing.Optional[str] = None):
+    async def cmd_kerk16(self, ctx: commands.Context, mention:typing.Optional[str] = None, *, message:typing.Optional[str] = None):
         #check if message in lounge
         if ctx.channel.name == ("testbot"):
             #check if user mentioned  
             if mention:
                 #set a task to check if it's 16:00 and posts message
-                while not (datetime.now().hour == 16 and datetime.now().minute == 00):
-                    await asyncio.sleep(59)
+                #while not (datetime.now().hour == 16 and datetime.now().minute == 00):
+                #    await asyncio.sleep(59)
                 msg = "⛪ Hey {0} het is 4u en <@{1}> wil met u vechten aan de kerk in Hamont! 👊".format(mention,ctx.author.id)
+                if message:
+                    msg += "\n\t\"{0}\"".format(message)
                 return await ctx.send(msg)
 
 def setup(bot: commands.Bot):
