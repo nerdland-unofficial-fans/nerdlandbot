b3ce17f2d9ed082193d5f07edb512533c9b98926
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2020-11-14 00:09:54 +0100

    - initialize scheduler - add scheduler task - move get_latest_video to scheduler

--- Commands/youtube.py
+++ Commands/youtube.py
@@ -1,5 +1,5 @@
-from discord.ext import commands
-from .GuildData import get_guild_data, GuildData
+from discord.ext import commands, tasks
+from .GuildData import get_all_guilds_data, get_guild_data, GuildData
 from Helpers.parser import parse_channel
 from Helpers.log import info, fatal
 from Translations.Translations import get_text as translate
@@ -8,8 +8,11 @@ import discord
 import requests
 import os
 
+from Scheduler.Scheduler import get_latest_video
+
 
 class Youtube(commands.Cog, name="Youtube_lists"):
+
     @commands.command(
         name="youtube_add",
         aliases=[],
@@ -94,45 +97,13 @@ class Youtube(commands.Cog, name="Youtube_lists"):
 
         guild_data = await get_guild_data(ctx.message.guild.id)
         msg = translate("youtube_list_title", await culture(ctx))
-        print(
-            str(
-                guild_data.youtube_channels["UCPjHlmSGP-rMg5PR-PyaJug"][
-                    "text_channel_id"
-                ]
-            )
-        )
+
         for channel_id, channel_data in guild_data.youtube_channels.items():
             msg = (
                 msg
                 + f"\n - Channel `channel_id` posts in <#{channel_data['text_channel_id']}>, last video ID: `{channel_data['latest_video_id']}`"
             )
         await ctx.send(msg)
-
-    async def get_latest_video(self, youtube_channel_id: str):
-        TOKEN = os.getenv("YOUTUBE_TOKEN")
-        if TOKEN:
-            info(f"Get latest videos for channel {youtube_channel_id}")
-        else:
-            fatal("Please provide a YOUTUBE_TOKEN in your .env file")
-            raise Exception("YouTube token is not set")
-
-        r = requests.get(
-            f"https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={youtube_channel_id}&maxResults=10&order=date&type=video&key={TOKEN}"
-        )
-        response = r.json()["items"][0]
-
-        video_id = response["id"]["videoId"]
-        title = response["snippet"]["title"]
-        description = response["snippet"]["description"]
-        link = f"https://www.youtube.com/watch?v={video_id}"
-
-        return {
-            "video_id": video_id,
-            "title": title,
-            "description": description,
-            "link": link,
-        }
-
 
 def setup(bot: commands.bot):
     bot.add_cog(Youtube(bot))
