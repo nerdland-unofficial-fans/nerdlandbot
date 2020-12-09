93421052b28ef55ead6d8af14e7920cd3f34ba09
Autom <mijnnaamis2112@hotmail.com>
2020-12-10 00:35:04 +0100

    Fix wombat pic folder and send message if no pictures were found

--- nerdlandbot/commands/wombat.py
+++ nerdlandbot/commands/wombat.py
@@ -4,6 +4,11 @@ import random
 
 from discord.ext import commands
 from nerdlandbot.helpers.constants import WOMBATS_DIR_NAME
+from nerdlandbot.helpers.TranslationHelper import get_culture_from_context as culture
+from nerdlandbot.translations.Translations import get_text as translate
+
+if not os.path.exists(WOMBATS_DIR_NAME):
+    os.makedirs(WOMBATS_DIR_NAME)
 
 class Wombat(commands.Cog, name="Wombat"):
 
@@ -12,11 +17,11 @@ class Wombat(commands.Cog, name="Wombat"):
 
     @commands.command(name="wombat_pic", brief="Post a random wombat picture", help="The bot will search for a random picture of a wombat in its database, and post it in the chat")
     async def cmd_wombat_pic(self, ctx):
+        wombat_list = [os.path.join(WOMBATS_DIR_NAME, w) for w in os.listdir(WOMBATS_DIR_NAME)]
 
-        parent_dir = os.path.join(os.path.dirname(__file__), os.pardir)
-        wombats_dir = os.path.join(parent_dir, WOMBATS_DIR_NAME)
-
-        wombat_list = [os.path.join(wombats_dir, w) for w in os.listdir(wombats_dir)]
+        if not wombat_list:
+            msg = translate("empty_wombat_list", await culture(ctx))
+            return await ctx.send(msg)
 
         await ctx.send(file=discord.File(random.choice(wombat_list)))
 
