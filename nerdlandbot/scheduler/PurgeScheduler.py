9de0c85407a005f83d7eee1d698b41d0f2409103
Eva774 <evasmeets94@gmail.com>
2021-02-06 12:13:36 +0100

    work around the limit for channel.purge

--- nerdlandbot/scheduler/PurgeScheduler.py
+++ nerdlandbot/scheduler/PurgeScheduler.py
@@ -23,7 +23,9 @@ async def purge_messages(bot):
                 channel = bot.get_channel(int(text_channel))
                 try:
                     if channel:
-                        await channel.purge(check=check, before=before)
+                        deleted_messages = [1]
+                        while len(deleted_messages) > 0:
+                            deleted_messages = await channel.purge(check=check, before=before)
                 except:
                     fatal(
                         f"Failed to purge messages for channel {channel.name} in guild {guild_data.guild_id}. Does the bot have appropriate permissions on that channel?"
