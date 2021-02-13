86a04bc13018f8dda6e6b19a8387973af82baf7b
Storm <61149527+yannicfreson@users.noreply.github.com>
2021-02-13 18:28:39 +0100

    eva *zucht*

--- nerdlandbot/commands/dadJoke.py
+++ nerdlandbot/commands/dadJoke.py
@@ -1,11 +1,7 @@
-import typing
-import asyncio
 import discord
 import requests
 
 from discord.ext import commands
-from nerdlandbot.translations.Translations import get_text as translate
-from nerdlandbot.helpers.TranslationHelper import get_culture_from_context as culture
 
 
 class DadJoke(commands.Cog, name="dadJoke"):
@@ -13,13 +9,11 @@ class DadJoke(commands.Cog, name="dadJoke"):
         self.bot = bot
 
     @commands.command(name="dadJoke", help="dadjoke_help")
-    async def cmd_dadJoke(self, ctx: commands.Context, mention:typing.Optional[str] = None, *, message:typing.Optional[str] = None): 
+    async def cmd_dadJoke(self, ctx: commands.Context): 
         r = requests.get('https://icanhazdadjoke.com',headers = { "Accept": "text/plain" })
         r.encoding = 'utf-8' 
         msg = r.text
-        if message:
-            msg += "\n\t\"{0}\"".format(message)
-        return await ctx.send(msg)
+        await ctx.send(msg)
 
 def setup(bot: commands.Bot):
     bot.add_cog(DadJoke(bot))
