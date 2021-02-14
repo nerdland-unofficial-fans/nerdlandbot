4ecb07ee53e6ae3fe0944403d53dcb1c9c0791f4
Storm <61149527+yannicfreson@users.noreply.github.com>
2021-02-14 01:35:42 +0100

    resolved comments

--- nerdlandbot/__main__.py
+++ nerdlandbot/__main__.py
@@ -47,7 +47,7 @@ bot.load_extension("nerdlandbot.commands.youtube")
 bot.load_extension("nerdlandbot.commands.poll")
 bot.load_extension("nerdlandbot.commands.purger")
 bot.load_extension("nerdlandbot.commands.kerk")
-bot.load_extension("nerdlandbot.commands.dadJoke")
+bot.load_extension("nerdlandbot.commands.dad_joke")
 
 # Initialize and start YouTube scheduler
 YOUTUBE_TOKEN = os.getenv("YOUTUBE_TOKEN")
