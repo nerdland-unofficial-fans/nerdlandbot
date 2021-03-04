ff9b2d07228e44181b1b9bb688b90c9f3c336ba1
LeoVDD <leovdd@hotmail.comm>
2021-03-04 23:45:51 +0100

    Centralize all ctx.send calls + Error embed

--- nerdlandbot/commands/space_launches.py
+++ nerdlandbot/commands/space_launches.py
@@ -16,14 +16,11 @@ from nerdlandbot.helpers.constants import THE_SPACE_DEVS_LOCAL_CACHE_SPACE_LAUNC
 class SpaceDevs (commands.Cog, name='The space devs'):
     def __init__(self,bot:commands.Bot):
         self.bot = bot
-        self.ctx = None
         self.cache_of_space_launches_json_path = os.path.join(THE_SPACE_DEVS_LOCAL_CACHE_FOLDER, THE_SPACE_DEVS_LOCAL_CACHE_SPACE_LAUNCHES_FILE + '.json')
         self.cache_of_space_launches_time_path = os.path.join (THE_SPACE_DEVS_LOCAL_CACHE_FOLDER, THE_SPACE_DEVS_LOCAL_CACHE_SPACE_LAUNCHES_FILE + '.time')
 
     @commands.command(name="space_launches", hidden = False, help="space_launches_help", brief="space_launches_brief")
     async def cmd_space_launches(self, ctx:commands.Context):
-        self.ctx = ctx
-
         full_url = '/'.join ([THE_SPACE_DEVS_BASE_URL, THE_SPACE_DEVS_VERSION, '/'.join (THE_SPACE_DEVS_UPCOMING_LAUNCH_RESOURCE)])
 
         if self.should_call_the_api ():
@@ -36,16 +33,16 @@ class SpaceDevs (commands.Cog, name='The space devs'):
                         with open(self.cache_of_space_launches_time_path,"w") as file:
                             timestamp = datetime.now().strftime(THE_SPACE_DEVS_TIMESTAMP_FORMAT)
                             file.write(timestamp)
-                        await self.parse_and_send_results(msg)
+                        await ctx.send(embed = await self.parse_and_compose_embed(msg))
                     elif resp.status == 429:
-                        await ctx.send('Too many requests. Wait a couple of minutes and try again.')
+                        await ctx.send(embed = await self.compose_error_embed('Too many requests. Wait a couple of minutes and try again.'))
                     else:
-                        await ctx.send('Call to the space devs failed. Response status: '+ str(resp.status))
+                        await ctx.send(embed = await self.compose_error_embed('Call to the space devs failed. Response status: ' + str(resp.status)))
                     return
         else:
             with open(self.cache_of_space_launches_json_path,"r") as file:
                 msg = file.read()
-            await self.parse_and_send_results(msg)
+            await ctx.send(embed = await self.parse_and_compose_embed(msg))
 
     def should_call_the_api (self):
         if os.path.isfile(self.cache_of_space_launches_json_path) and os.path.isfile(self.cache_of_space_launches_time_path):
@@ -56,17 +53,20 @@ class SpaceDevs (commands.Cog, name='The space devs'):
         else:
             return True
           
-    async def parse_and_send_results(self, json_string):    # extracted for testing purposes
+    async def parse_and_compose_embed(self, json_string):    # extracted for testing purposes      
         try:
             dom = json.loads(json_string)
         except json.JSONDecodeError:
-            await self.ctx.send('Could not parse the response from space devs.')
-            return
+            return await self.compose_error_embed('Could not parse the response from space devs.')
         embed = self.main_info_embed()
         for index, result in enumerate (dom['results']):
             await self.get_embed_field_for_upcominglaunch(index, result, embed)
-        await self.ctx.send(embed = embed)
-        return
+        return embed
+
+    async def compose_error_embed(self, error_msg):
+        embed = self.main_info_embed()
+        embed.add_field(name = 'ERROR:', value= error_msg, inline=True)
+        return embed
 
     def main_info_embed (self):
         result = discord.Embed(
@@ -92,24 +92,21 @@ class SpaceDevs (commands.Cog, name='The space devs'):
             try:                
                 mission = result_json["mission"]["name"]
             except TypeError:
-                mission = "No mission"
-            
+                mission = "No mission"            
             try:
                 rocket_configuration = result_json["rocket"]["configuration"]["name"]
             except TypeError:
                 rocket_configuration = "No configuration"
-            
             try:
                 pad_location = result_json["pad"]["location"]["name"]
             except TypeError:
                 pad_location = "No location"
         except KeyError:
-            return ' '.join ([index+1, 'Could not parse launch data.'])
-
-
+            embed.add_field(name = 'ERROR:', value= ' '.join ([index+1, 'Could not parse launch data.']),inline = False)
         name = str(index+1) + '. ' +service_provider+' : '+ mission+ '  ('+ windows_start +') '
         value =  '; '.join([pad_location, rocket_configuration])
         embed.add_field(name = name, value = value, inline = False)
+        return
 
 def setup(bot: commands.Bot):
     bot.add_cog(SpaceDevs(bot))
