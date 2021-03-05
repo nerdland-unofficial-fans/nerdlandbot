a2af70295fbc81f166a25e8df636517418125228
Eva774 <evasmeets94@gmail.com>
2021-03-05 21:07:09 +0100

    Revert "Change bot status, added poncho command and grouped commands that post a random pic or message"
    This reverts commit a5cafac17043d2af1f7ae26c1ae8ed8056f836da.

--- nerdlandbot/eventhandlers/onready.py
+++ nerdlandbot/eventhandlers/onready.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-import discord
+
 from nerdlandbot.helpers.log import info
 
 
@@ -13,7 +13,7 @@ class OnReady(commands.Cog, name="on_ready"):
         This gets executed when the bot connects to discord.
         """
         info(f'{self.bot.user.name} has connected to Discord!')
-        await self.bot.change_presence(activity=discord.Game(name=f"with {self.bot.command_prefix}poncho"))
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(OnReady(bot))
