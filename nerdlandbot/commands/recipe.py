d96445a4638ecb1495f53bb66f0c325b7b516380
Gianni <selleslagh.gianni@gmail.com>
2021-03-16 03:48:50 +0100

    Added translations and succes-checks.

--- nerdlandbot/commands/recipe.py
+++ nerdlandbot/commands/recipe.py
@@ -19,17 +19,13 @@ class Recipe(commands.Cog, name="recipes"):
         self.bot = bot
     
 
-    @commands.command(name="add_recipe")
+    @commands.command(name="add_recipe", brief="recipe_brief", help="recipe_help")
     async def add_recipe(self, ctx: commands.Context):
         # Getting everything ready to acces 
         gc = gspread.service_account(SHEETS_TOKEN)
         sh = gc.open(SPREADSHEET)
         ws = sh.sheet1
         next_row = next_available_row(ws)
-
-        # TODO: Add this and other text in translations.csv
-        questions = ["the name of the dish", "the link to the recipe", "a rating", "comments", "your name"]
-        answers = []
 
         # Fetching date and formatting it
         d_obj = datetime.now()
@@ -40,15 +36,27 @@ class Recipe(commands.Cog, name="recipes"):
                 return message.author == author
             return inner_check
         
+        lang = await culture(ctx)
+        questions = []
+        answers = []
+        
         # Asking the user questions and capturing the answer
-        for i in range(len(questions)):
-            await ctx.send("Please enter {}".format(questions[i]))
+        for i in range(5):
+            questions.append(translate("recipe_template", lang).format(translate("recipe_{}_question".format(i+1), lang)))
+            await ctx.send(questions[i])
             await asyncio.sleep(1)
             try:
-                reaction = await ctx.bot.wait_for("message", timeout=20, check=check(ctx.author))
+                reaction = await ctx.bot.wait_for("message", timeout=30, check=check(ctx.author))
                 answers.append(reaction)
+                if i == 2:
+                    try:
+                        int(reaction.content)
+                    except:
+                        int_error = translate("recipe_int_error", lang)
+                        return await ctx.send(int_error)
             except asyncio.TimeoutError:
-                return await ctx.send("Go to bed, sleepy!")
+                timeout = translate("recipe_timeout", lang)
+                return await ctx.send(timeout)
 
         # Updating the worksheet(ws) with all the data asked of the user
         ws.update("A{}".format(next_row), date_string)
@@ -60,11 +68,22 @@ class Recipe(commands.Cog, name="recipes"):
         ws.update("E{}".format(next_row), answers[3].content)
         ws.update("F{}".format(next_row), answers[4].content)
 
+        # On a delay of 5 seconds (google isn't instant) check if the last cell was added
+        await asyncio.sleep(5)
+        if not ws.acell("F{}".format(next_row)).value:
+            error_msg = translate("recipe_error", lang)
+            return await ctx.send(error_msg)
+        else:
+            succes_msg = translate("recipe_succes", lang)
+            return await ctx.send(succes_msg)
+
+
 
 def next_available_row(worksheet) -> str:
     """
     Returning the next available row
-    worksheet: the sheet you're working on
+    :param worksheet: The worksheet you're working on
+    :return: The index of the next available column
     """
     str_list = list(filter(None, worksheet.col_values(1)))
     return str(len(str_list)+1)
