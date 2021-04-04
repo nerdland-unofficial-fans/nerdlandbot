14a68cb8bc4669a867f404cefb1e723b770c76ce
Gianni <selleslagh.gianni@gmail.com>
2021-04-04 15:17:57 +0200

    Removed replying from all commands except for eight_ball

--- nerdlandbot/commands/random.py
+++ nerdlandbot/commands/random.py
@@ -57,13 +57,13 @@ class Random(commands.Cog, name="Random"):
         # Error for missing parameter
         if channel_name is None:
             msg = translate("random_user_no_channel", await culture(ctx))
-            return await ctx.reply(msg)
+            return await ctx.send(msg)
 
         # Choose from online members if requested
         if channel_name == "online":
             member = pick_random_online_member(ctx)
             msg = translate("random_user_chosen", await culture(ctx)).format(member.id)
-            return await ctx.reply(msg)
+            return await ctx.send(msg)
 
         # Sanitize channel name
         channel = get_channel(ctx,channel_name)
@@ -71,17 +71,17 @@ class Random(commands.Cog, name="Random"):
         # Error if channel does not exist
         if channel is None:
             msg = translate("membercount_channel_nonexistant", await culture(ctx))
-            return await ctx.reply(msg)
+            return await ctx.send(msg)
 
         # Error if channel empty
         if len(channel.members) < 1:
             msg = translate("membercount_empty_channel", await culture(ctx)).format(channel.id)
-            return await ctx.reply(msg)
+            return await ctx.send(msg)
 
         # Pick a random user from channel, and report back to user
         member = random.choice(channel.members)
         msg = translate("random_user_chosen", await culture(ctx)).format(member.id)
-        await ctx.reply(msg)
+        await ctx.send(msg)
 
     @commands.command(name="wombat_pic", aliases = ["wombat","wombatpic"], hidden=True, help="wombat_pic_help")
     async def cmd_wombat_pic(self, ctx):
@@ -89,9 +89,9 @@ class Random(commands.Cog, name="Random"):
 
         if not wombat_list:
             msg = translate("empty_wombat_list", await culture(ctx))
-            return await ctx.reply(msg)
+            return await ctx.send(msg)
 
-        await ctx.reply(file=discord.File(random.choice(wombat_list)))
+        await ctx.send(file=discord.File(random.choice(wombat_list)))
 
     @commands.command(name="poncho", aliases=["poncho_pic","ponchopic"],hidden=True, help="poncho_pic_help")
     async def cmd_poncho_pic(self, ctx):
@@ -99,10 +99,10 @@ class Random(commands.Cog, name="Random"):
 
         if not poncho_list:
             msg = translate("empty_poncho_list", await culture(ctx))
-            return await ctx.reply(msg)
+            return await ctx.send(msg)
 
         msg = translate("poncho_hond", await culture(ctx))
-        await ctx.reply(msg)
+        await ctx.send(msg)
         await ctx.send(file=discord.File(random.choice(poncho_list)))
 
     @commands.command(name="eight_ball", aliases=["eightball"], help="eight_ball_help")
@@ -121,7 +121,7 @@ class Random(commands.Cog, name="Random"):
 
                 # msg can contain utf 2028 charactercode which is line-separator, we're just converting it to '\n'
                 msg = '\n'.join(msg.splitlines())
-                await ctx.reply(msg)
+                await ctx.send(msg)
 
 def setup(bot):
     bot.add_cog(Random(bot))
