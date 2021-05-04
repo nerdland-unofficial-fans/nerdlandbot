82132c4df7ff1deea2fc13797ee5ca81853d3dfb
Gianni Selleslagh <selleslagh.gianni@gmail.com>
2021-05-04 16:35:28 +0200

    Optimized the scheduler and made church_message gender neutral.

--- nerdlandbot/scheduler/ChurchScheduler.py
+++ nerdlandbot/scheduler/ChurchScheduler.py
@@ -12,23 +12,26 @@ from nerdlandbot.helpers.TranslationHelper import get_culture_from_context as cu
 from nerdlandbot.helpers.emoji import fist, church_emoji
 from nerdlandbot.helpers.log import warn
 
-@tasks.loop(minutes = 1.0)
+@tasks.loop(minutes=1.0)
 async def church_fights(bot):
-    guilds_data = await get_all_guilds_data()
-    day = datetime.now().day
-    hour = datetime.now().hour
-    for guild_data in guilds_data:
-        channel_id = guild_data.church_channel
-        try:
-            channel = bot.get_channel(channel_id)
-        except:
-            warn("Something went wrong. It's possible the channel is not set.")
-        for church in guild_data.church_event:
-            # For every church_event scheduled, send it to the kerk channel
-            for _ in range(len(guild_data.church_event)):
-                if day == church["day"] and hour == 16:
-                    msg = translate("church", church["culture"]).format(church_emoji, church["receiver"], church["sender"], fist)
-                    if church["message"]:
-                        msg += translate("church_message", church["culture"]).format(church["message"])
-                    await channel.send(msg)
-                    await guild_data.remove_church_event()
+	current_time = datetime.now()
+	if current_time.hour == 16 and current_time.minute == 0:
+		guilds_data = await get_all_guilds_data()
+		day = current_time.day
+		for guild_data in guilds_data:
+			channel_id = guild_data.church_channel
+			try:
+				channel = bot.get_channel(channel_id)
+			except:
+				warn("Something went wrong. It's possible the channel is not set.")
+			for church in guild_data.church_event:
+				# For every church_event scheduled, send it to the kerk channel
+				for _ in range(len(guild_data.church_event)):
+					if day == church["day"]:
+						msg = translate("church", church["culture"]).format(
+							church_emoji, church["receiver"], church["sender"], fist)
+						if church["message"]:
+							msg += translate("church_message",
+											church["culture"]).format(church["message"])
+						await channel.send(msg)
+						await guild_data.remove_church_event()
