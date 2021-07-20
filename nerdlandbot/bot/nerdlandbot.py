b15abf1de8bd8bc0b607300c7fa32df59c8e9c90
Autom <mijnnaamis2112@hotmail.com>
2021-07-20 23:08:31 +0200

    Overwrite process_commands and remove bot check

--- nerdlandbot/bot/nerdlandbot.py
+++ nerdlandbot/bot/nerdlandbot.py
@@ -6,3 +6,11 @@ class NerdlandBot(Bot):
     def __init__(self, prefix: str, intents: Intents):
         self.prefix = prefix
         super().__init__(command_prefix=self.prefix, intents=intents, case_insensitive=True,)
+
+    async def process_commands(self, message):
+        # # Intentionally don't ignore other bots' messages
+        # if message.author.bot:
+        #     return
+
+        ctx = await self.get_context(message)
+        await self.invoke(ctx)
